b1090c043ec0e3ab6deed3c9656974cbf2597106
Zagreus <artemij.ca@gmail.com>
2025-01-21 16:26:44 -0500

    Chaged to backened and frontend tructure

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to my Portfolio');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
