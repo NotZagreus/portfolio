b9c98d53021db7fc29af4d65a1e2661789a22068
Zagreus <artemij.ca@gmail.com>
2025-02-09 22:19:19 -0500

    progress

---
+++ frontend/src/Portfolio.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="left-side">
+      <Contact />
+      <div class="info-container">
+        <h1>Artem Kozlov</h1>
+        <h3>3rd Year Computer Science Student</h3>
+        <h3>I specialize in full-stack development, IT support and love cybersecurity.</h3>
+      </div>
+      <div class="navigation-container">
+      </div>
+    </div>
+    <div class="right-side">
+      Right side
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Contact from './components/Contact.vue'
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,17 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/portfolio.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+// import App from './App.vue'
 import router from './router'
 import auth0 from './plugins/auth0'
+import Portfolio from './Portfolio.vue'
 
-const app = createApp(App)
+// const app = createApp(App)
+
+const app = createApp(Portfolio)
 
 app.use(createPinia())
 app.use(router)
